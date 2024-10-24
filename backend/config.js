bd0594246e8451f10fc84a765a29776cc7580f3d
SarahSmallwood <133904630+SarahSmallwood@users.noreply.github.com>
2024-10-23 22:13:57 -0700

    feat: adding POST book endpoint

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://sarahsmallwood56:jl2byHR1DZuGvI9Q@bookstoremern.h1rc9.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookstoreMern'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,64 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from './models/bookModel.js';
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-})
+// Middleware for Parsing request body
+
+app.use(express.json());
+
+app.get(`/`, (request, response) => {
+    console.log(request);
+    return  response.status(234).send(`Welcome to MERN stack Tutorial`);
+});
+
+//Route for Save a New Book
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: `Send all required fields: title, author, publishYear`,
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+       console.log(error.message);
+       response.status(500).send({ message: error.message});
+    }
+});
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log(`App connected to database`);
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+// M.m.p
+// Major.minor.patch
+// Semantic versioning
+// Major -> Breaking change in the app (CM: 'feat!: ...')
+// minor -> New feature in the app (CM: 'feat: ...)
+// patch -> Fix/Small change in the app that doesn't add 
+//          new value (e.g. Change text size)
+//          (CM: 'fix/chore/hotfix: ...')
